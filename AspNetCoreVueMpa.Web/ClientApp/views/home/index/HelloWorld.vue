8e3851250ca5c5caebe7067fda262a5a7a6137ad
kajan <kajan@tsp.dev>
2021-02-12 20:45:12 +0530

    add `HelloWorld.vue`

---
+++ AspNetCoreVueMpa.Web/ClientApp/views/home/index/HelloWorld.vue
@@ -0,0 +1,20 @@
+﻿<template>
+  <div>
+    Hello {{ name }} from Vue! 
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      name: "world",
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
