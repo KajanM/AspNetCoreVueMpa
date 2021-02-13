2290f5614dd11b03a7efad1ad40d22ffb9b47b0e
kajan <kajan@tsp.dev>
2021-02-13 05:34:16 +0530

    dynamically add webpack entries

--- AspNetCoreVueMpa.Web/ClientApp/webpack.config.js
+++ AspNetCoreVueMpa.Web/ClientApp/webpack.config.js
@@ -1,6 +1,23 @@
 ﻿const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const glob = require('glob');
+
+const entries = {};
+const IGNORE_PATHS = ['unused'];
+
+glob.sync('./views/**/main.js').forEach(path => {
+  const chunk = path.split('./views/')[1].split('/main.js')[0]
+  if (IGNORE_PATHS.every(path => !chunk.includes(path))) {
+    if (!chunk.includes('/')) {
+      entries[chunk] = path
+    } else {
+      const joinChunk = chunk.split('/').join('-')
+      entries[joinChunk] = path
+    }
+  }
+});
 
 module.exports = {
+  entry: entries,
   module: {
     rules: [
       {
