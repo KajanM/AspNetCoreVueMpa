cfa53014b40815d90940457789e11fbc66e9ac17
kajan <kajan@tsp.dev>
2021-02-14 16:05:31 +0530

    move shared app initialization logic to app-init.js

---
+++ AspNetCoreVueMpa.Web/ClientApp/utils/app-init.js
@@ -0,0 +1,6 @@
+﻿import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
+
+Vue.use(BootstrapVue);
+
+Vue.component('default-layout', () => import(/* webpackChunkName: "layout-container" */ '../components/layout/DefaultLayout.vue'));
