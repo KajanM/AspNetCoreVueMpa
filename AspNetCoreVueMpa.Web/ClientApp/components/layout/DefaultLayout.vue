db38ac2d44e9e938ca1a171783d9fed8f5244f2f
kajan <kajan@tsp.dev>
2021-02-14 15:51:50 +0530

    add layout component

---
+++ AspNetCoreVueMpa.Web/ClientApp/components/layout/DefaultLayout.vue
@@ -0,0 +1,23 @@
+﻿<template>
+  <div>
+    <nav>
+      Nav goes here
+    </nav>
+    <main>
+      <slot></slot>
+    </main>
+    <footer>
+      Footer goes here
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LayoutContainer"
+}
+</script>
+
+<style scoped>
+
+</style>
