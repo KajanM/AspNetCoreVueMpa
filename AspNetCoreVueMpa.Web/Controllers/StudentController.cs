94a8f5709fbc1704904fe09f60656f108374150c
kajan <kajan@tsp.dev>
2021-02-14 05:59:02 +0530

    add StudentController

---
+++ AspNetCoreVueMpa.Web/Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreVueMpa.Web.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
