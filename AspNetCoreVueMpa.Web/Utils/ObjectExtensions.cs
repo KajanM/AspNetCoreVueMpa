63ffaaea9d4c2a79adf94156befed44e820b53dd
kajan <kajan@tsp.dev>
2021-02-14 18:28:31 +0530

    add `ToJson` extension method

---
+++ AspNetCoreVueMpa.Web/Utils/ObjectExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace AspNetCoreVueMpa.Web.Utils
+{
+    public static class ObjectExtensions
+    {
+        public static string ToJson(this object obj)
+        {
+            var settings = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                NullValueHandling = NullValueHandling.Ignore,
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            };
+            return JsonConvert.SerializeObject(obj, settings);
+        }
+    }
+}
